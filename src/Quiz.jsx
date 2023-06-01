9118c2760d92ac27cb4ebe0025e8e534d7b2baaa
SophieNordell <113043401+SophieNordell@users.noreply.github.com>
2023-06-01 13:45:14 +0200

    ändrat färger

--- src/Quiz.jsx
+++ src/Quiz.jsx
@@ -5,22 +5,22 @@ function Quiz() {
   const currentQuestionn = questions[currentQuestion];
 
   return (
-    <div className="flex flex-col space-y-4 w-fit p-10  ">
+    <div className="flex flex-col space-y-4 w-fit p-10">
       {currentQuestionn.title}
       <button
-        className="bg-purple-300 text-green-50 rounded text-xl p-2 mt-4 hover:bg-purple-500 "
+        className="bg-purple-300 text-green-50 rounded text-xl p-2 mt-5 hover:bg-purple-500 "
         onClick={() => answerQuestion(1)}
       >
         {currentQuestionn.alt1}
       </button>
       <button
-        className="bg-purple-300 text-green-50 rounded text-xl p-2  hover:bg-purple-500  "
+        className="bg-purple-300 text-green-50 rounded text-xl p-2 mt-5  hover:bg-purple-500  "
         onClick={() => answerQuestion(2)}
       >
         {currentQuestionn.alt2}
       </button>
       <button
-        className="bg-purple-300 text-green-50 rounded text-xl p-2  hover:bg-purple-500 "
+        className="bg-purple-300 text-green-50 rounded text-xl p-2 mt-5  hover:bg-purple-500 "
         onClick={() => answerQuestion(3)}
       >
         {currentQuestionn.alt3}
